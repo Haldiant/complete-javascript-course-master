fe7980b8a5d469c220582088971330a17f853c44
Haldiant <nhalwidinato@gmail.com>
2024-06-20 21:24:15 +0700

    33. Function

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log("i can drive");
+
+// const interface = "audio";
+// const private = 534;
+// const if = 23;
+
+// 33. Function
+function logger() {
+  console.log("my name is nahal");
+}
+
+// calling / running / invoking function
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
